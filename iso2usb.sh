bf036e291aa2704b7dc6e41ce9aad1295e3af1e2
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2019-01-20 00:32:32 +0100

    change in writing approach to etcher

--- iso2usb.sh
+++ iso2usb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 if [ $# -ne 2 ]
 then
   echo "Script $0 require device and ifo files to be provided as arguments:"
@@ -19,13 +20,5 @@ then
   exit 3
 fi
 
-mkfs.vfat -nUBUNTUUEFI $1
-mount $1 /mnt/usb
-7z x $2 -o/mnt/usb/
-
-ls /mnt/usb/
-
-umount /mnt/usb
-
-#sudo dd if=linuxium-v4.20-ubuntu-18.04.1-desktop-amd64.iso of=/dev/sdc bs=4M status=progress
+sudo /opt/etcher-cli/etcher -d $1 -y $2
 
